efb45ca1a39e5d091420221346792ea00bcdff5f
odgr-show <121861788+odgr-show@users.noreply.github.com>
2024-02-01 21:30:33 +0900

    ポートフォリオが完成しました。

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+export const BlogPage = () => {
+    return (
+        <div className="container">
+            <h1>ブログ</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fugiat aut quos
+                inventore blanditiis consectetur quisquam dolorem neque molestiae iure iusto culpa
+                accusamus quod repellendus minima dolores praesentium ab? Dolore sequi, quia
+                blanditiis dignissimos incidunt dicta repellendus quidem, vitae inventore ea rerum
+                eos nisi, soluta commodi. Nobis dolore aliquid, atque impedit, fugiat laudantium
+                veritatis dolorem voluptate blanditiis saepe asperiores doloribus incidunt modi
+                tempora qui. Officiis magnam deserunt soluta molestias odio iusto, tempora, corrupti
+                tempore fugit praesentium quas, cumque quasi ipsa rem quibusdam nobis temporibus.
+                Ipsam, expedita molestias nobis enim in possimus est debitis aliquid quod
+                consequatur mollitia dolorem corrupti! Ab.
+            </p>
+        </div>
+    );
+};
+
+export default BlogPage;
